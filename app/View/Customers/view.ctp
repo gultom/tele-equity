ce831d91f9c485f18033ef01636a6ebf2f9f9a6a
Charles Gultom <gultom.c@gmail.com>
2012-11-16 17:56:09 +0700

    update login, update main template

--- app/View/Customers/view.ctp
+++ app/View/Customers/view.ctp
@@ -1,1 +1,13 @@
-<?= 'kokokn'; ?>
+<?=
+    $this->Html->script(array(
+        'lib/jquery/jquery-1.8.2.min',
+        'lib/prototype/prototype',
+        'app/Users'
+    ));
+
+    $this->Html->scriptBlock(
+        '
+        var Users = new Users();
+        '
+    );
+?>
--- app/webroot/js/app/Users.js
+++ app/webroot/js/app/Users.js
@@ -1,15 +1,7 @@
 
 var Users = Class.create({
     
-    doLogin: function(formName) {
-        new Ajax.Request ('dologin', {
-            method: 'post',
-            parameters: Form.serialize(formName.toString()),
-            onSuccess: function(response) {
-                if (response.responseText === 'true') {
-                    window.location.href = '/tele-equity/customers/view';
-                }
-            }
-        })
+    confirmLogout: function() {
+        
     }
 });
