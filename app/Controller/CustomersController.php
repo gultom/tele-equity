5f3cdc70fe9ee80bb1ca62611ca60828f87ba776
Charles Gultom <gultom.c@gmail.com>
2012-11-19 16:14:15 +0700

    add menu

--- app/Controller/CustomersController.php
+++ app/Controller/CustomersController.php
@@ -5,8 +5,15 @@
  *
  * @author charles
  */
+
 class CustomersController extends AppController {
     
+    public function beforeFilter() {
+        parent::beforeFilter();
+        $userMenu = $this->requestAction('/users/getmenus');
+        $this->set('menu', $userMenu);
+    }
+
     public function view() {
         
     }
