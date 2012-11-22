c3211be066e609801eca700a3fd1f859d956bf7c
Charles Gultom <gultom.c@gmail.com>
2012-11-22 23:45:30 +0700

    add UserGroups Controller

---
+++ app/Controller/UserGroupsController.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Description of UserGroupsController
+ *
+ * @author Charles
+ */
+class UserGroupsController extends AppController {
+    
+    public function view() {
+        
+    }
+    
+    public function lists() {
+        $this->autoRender = false;
+        $groups = $this->UserGroup->find('list', array (
+            'fields' => array (
+                'UserGroup.id',
+                'UserGroup.group_name'
+            ),
+            'order' => array (
+                'UserGroup.group_name'
+            )
+        ));
+        return json_encode($groups);
+    }
+
+    public function add() {
+        
+    }
+    
+    public function edit() {
+        
+    }
+    
+    public function delete() {
+        
+    }
+}
+
+?>
