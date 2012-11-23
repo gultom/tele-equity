148757c8177407034776f2e8a178a362cdd6cff2
Charles Gultom <gultom.c@gmail.com>
2012-11-23 10:22:02 +0700

    added function to add option for Group Select box

--- app/Controller/UserGroupsController.php
+++ app/Controller/UserGroupsController.php
@@ -12,7 +12,8 @@ class UserGroupsController extends AppController {
     
     public function lists() {
         $this->autoRender = false;
-        $groups = $this->UserGroup->find('list', array (
+        $groups = array ('' => '(Choose One)');
+        $groups = array_merge($groups, $this->UserGroup->find('list', array (
             'fields' => array (
                 'UserGroup.id',
                 'UserGroup.group_name'
@@ -20,7 +21,7 @@ class UserGroupsController extends AppController {
             'order' => array (
                 'UserGroup.group_name'
             )
-        ));
+        )));
         return json_encode($groups);
     }
 
