54d9e1e169797396146a2bd709bed2c988d258e0
Charles Gultom <gultom.c@gmail.com>
2012-11-29 15:58:23 +0700

    add Models : - Call - Customer - CustomerResponse - CustomerStatus

---
+++ app/Model/CustomerResponse.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Description of CustomerResponse
+ *
+ * @author Charles
+ */
+class CustomerResponse extends AppModel {
+    
+    public $useTable = '_list_values';
+    public $primaryKey = 'list_code';
+}
+
+?>
