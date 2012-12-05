3ba09658c905496b35a16c129b162f4395a3780c
Charles Gultom <gultom.c@gmail.com>
2012-12-05 16:01:29 +0700

    Added UserGroup javascript class

---
+++ app/webroot/js/app/UserGroup.js
@@ -0,0 +1,17 @@
+
+var UserGroup = Class.create({
+    
+    id: null,
+    
+    setId: function(id) {
+        this.id = id;
+    },
+    
+    getId: function() {
+        return this.id;
+    },
+    
+    initShowGroupsDialog: function() {
+        
+    }
+});
