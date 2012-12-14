444a73fff9dc63d47c1a3d63e70b32ff5f22860b
Charles Gultom <gultom.c@gmail.com>
2012-12-14 18:30:02 +0700

    update

--- app/Controller/CustomersController.php
+++ app/Controller/CustomersController.php
@@ -145,7 +145,15 @@ class CustomersController extends AppController {
     }
     
     public function details($id) {
-        
+        $this->Customer->id = $id;
+        $this->request->data = $this->Customer->read();
+        $this->loadModel('Response');
+        $this->set('response', $this->Response->find('list', array (
+            'fields' => array (
+                'Response.id',
+                'Response.response'
+            )
+        )));
     }
 }
 
--- app/Model/Customer.php
+++ app/Model/Customer.php
@@ -10,19 +10,35 @@ class Customer extends AppModel {
     public $belongsTo = array (
         'TM' => array (
             'className' => 'User',
-            'foreignKey' => 'tm_id'
+            'foreignKey' => 'tm_id',
+            'fields' => array (
+                'TM.id',
+                'TM.username'
+            )
         ),
         'TL' => array (
             'className' => 'User',
-            'foreignKey' => 'tl_id'
+            'foreignKey' => 'tl_id',
+            'fields' => array (
+                'TL.id',
+                'TL.username'
+            )
         ),
         'SPV' => array (
             'className' => 'User',
-            'foreignKey' => 'spv_id'
+            'foreignKey' => 'spv_id',
+            'fields' => array (
+                'SPV.id',
+                'SPV.username'
+            )
         ),
         'QA' => array (
             'className' => 'User',
-            'foreignKey' => 'qa_id'
+            'foreignKey' => 'qa_id',
+            'fields' => array (
+                'QA.id',
+                'QA.username'
+            )
         ),
         'Status' => array (
             'className' => 'CustomerStatus',
